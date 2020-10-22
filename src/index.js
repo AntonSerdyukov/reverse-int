59f7e057606f48708f7708939c156650888b4a05
AntonSerdyukov <56315471+AntonSerdyukov@users.noreply.github.com>
2020-10-22 20:44:16 +0300

    fix: Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
+  let digits=Array.from(n.toString().replace('-','').split(''),Number); 
+  let reversedDigits=[];
+  
+  for (let i=0;i<digits.length;i++){
+    reversedDigits[i]=digits[digits.length-1-i];
+  }
+  
+  return Number(reversedDigits.join(''));
   
 }
